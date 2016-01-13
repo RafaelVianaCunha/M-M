08c8c5a164c72f4b2b669ab3face2ed62b43f67a
Rafael <rafael.cunha@al.infnet.edu.br>
2016-01-12 23:48:00 -0200

    atualizado

--- loja/models.py
+++ loja/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255, null=False)
+    descricao = models.CharField(max_length=15, null=False)
+    valor = models.CharField(max_length=255, null=False)
+    qtd = models.PositiveIntegerField(null=False)
+    descricao = models.TextField(null=False)
+    categoria = models.ManyToManyField(Categoria, related_name="categorias")
+    img = models.ManyToManyField(Imagem, related_name="imagens")
+    dataAdicionado = models.DateField(null=False)
+    qtdVendido = models.PositiveIntegerField(null=False)
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255, null=False)
+
+class Imagem(models.Model):
+    descricao = models.CharField(max_length=255, null=False)
--- loja/views.py
+++ loja/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from models import Produto
 
 # Create your views here.
 
 def index(request):
-     return render(request, 'index.html')
+     return render(request, 'index.html', { 'produtos' : Produto.objects.all()})
 
 def produto(request):
      return render(request, 'produto.html')
